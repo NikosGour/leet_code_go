f240ac4d97818336f15e3be4bc6039e19081a58f
NikosGour <nikosgournak@gmail.com>
2024-10-27 22:43:41 +0200

    solved palindrome

---
+++ src/palindrome.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	i := 0
+
+	for i < len(str)/2 {
+		if str[i] != str[len(str)-1-i] {
+			return false
+		}
+		i++
+	}
+
+	return true
+}
